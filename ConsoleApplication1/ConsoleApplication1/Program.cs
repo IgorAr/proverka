46b7593ef578d65001b7ceb9462926cb37889491
IgorAr <iarinichev@gmail.com>
2015-09-16 20:26:56 +0400

    Calc 16.09

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,33 +10,72 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Консольный калькулятор (Звездочка 1.0)");
+            var salmons = new List<string>();
+            salmons.Add("sum");
+            salmons.Add("sub");
+            salmons.Add("mult");
+            salmons.Add("div");
+
             while (true)
             {
-                Console.WriteLine("Консольный калькулятор (Звездочка 1.0)");
+                Console.WriteLine(@"sum [sub,div,mult] a b");
 
-                Console.WriteLine(@"Выберите арифметическое действие: 
-             - Умножение (введите *) 
-             - Деление (введите /) 
-             - Сложение (введите +) 
-             - Вычитание (введите -)");
                 string s = Console.ReadLine();
-                float a, b;
-                Console.WriteLine(' ');
-                Console.WriteLine("Введите первое число");
-                a = float.Parse(Console.ReadLine());
-                Console.WriteLine("Введите второе число");
-                b = float.Parse(Console.ReadLine());
-                Console.WriteLine(' ');
-                if (s == "*")
-                    Console.WriteLine("Результат умножения = {0}", a * b);
-                if (s == "/")
-                    Console.WriteLine("Результат деления ={0} ", a / b);
-                if (s == "+")
-                    Console.WriteLine("Результат сложения ={0} ", a + b);
-                if (s == "-")
-                    Console.WriteLine("Результат вычитания ={0} ", a - b);
+                string[] split = s.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // CHECK user input
+                if (split.Length != 3)
+                {
+                    Console.WriteLine("You inputed too many symbols, see input format");
+                    continue;
+                }
+                if (!salmons.Contains(split[0]))
+                {
+                    Console.WriteLine("Please input correct operation");
+                    continue;
+                };
+
+                float a;
+                bool result1 = float.TryParse(split[1], out a);
+                if (!(result1))
+                {
+                    Console.WriteLine(split[1] + " is not a number");
+                    continue;
+                }
+                float b;
+                bool result2 = float.TryParse(split[2], out b);
+                if (!(result2))
+                {
+                    Console.WriteLine(split[2] + " is not a number");
+                    continue;
+                }
+                //Output and calculating
+
+                switch (split[0])
+                {
+                    case "mult":
+                        Console.WriteLine("Результат умножения = {0}", a * b);
+                        break;
+                    case "div":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Division by zero");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Результат деления ={0} ", a / b);
+                        }
+                        break;
+                    case "sum":
+                        Console.WriteLine("Результат сложения ={0} ", a + b);
+                        break;
+                    case "sub":
+                        Console.WriteLine("Результат вычитания ={0} ", a - b);
+                        break;
+                }
+
                 Console.ReadKey();
-                Console.Clear();
             }
         }
     }
---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Консольный калькулятор (Звездочка 2.0)");
+
+                Console.WriteLine(@"Выберите арифметическое действие: 
+             - Умножение mult a b 
+             - Деление div a b
+             - Сложение sum a b
+             - Вычитание dif a b");
+                /*
+                string s = Console.ReadLine();
+                float a, b;
+                Console.WriteLine(' ');
+                Console.WriteLine("Введите первое число");
+                a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                b = float.Parse(Console.ReadLine());
+                Console.WriteLine(' ');
+                if (s == "mult")
+                    Console.WriteLine("Результат умножения = {0}", a * b);
+                if (s == "div")
+                    Console.WriteLine("Результат деления ={0} ", a / b);
+                if (s == "sum")
+                    Console.WriteLine("Результат сложения ={0} ", a + b);
+                if (s == "dif")
+                    Console.WriteLine("Результат вычитания ={0} ", a - b);
+                Console.ReadKey();
+                Console.Clear();
+                */
+            }
+        }
+    }
+}
